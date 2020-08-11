ec2f25e39eef1a2d9f2a13f19c69b66e0817a6a1
Drew Brown <drew@drewbrown.me>
2020-08-10 22:32:32 -0600

    Complete Part 4
    Add  animations (poses, walking, flipping)
    Use friendly property syntax for `GetNode<>()`

--- Steve.cs
+++ Steve.cs
@@ -6,17 +6,36 @@ public class Steve : KinematicBody2D
 	const int SPEED = 180;
 	const int GRAVITY = 35;
 	const int JUMPFORCE = -1100;
-    
-    private Vector2 velocity;
-	public override void _PhysicsProcess(float delta) {
-		if (Input.IsActionPressed("right")) {
+
+	private Vector2 velocity;
+
+	public AnimatedSprite Sprite { get => GetNode<AnimatedSprite>("Sprite"); }
+	public override void _PhysicsProcess(float delta)
+	{
+		if (Input.IsActionPressed("right"))
+		{
 			velocity.x = SPEED;
+			Sprite.Play("walk");
+			Sprite.FlipH = false;
 		}
-		else if (Input.IsActionPressed("left")) {
+		else if (Input.IsActionPressed("left"))
+		{
 			velocity.x = -SPEED;
+			Sprite.Play("walk");
+			Sprite.FlipH = true;
+		}
+		else
+		{
+			Sprite.Play("idle");
+
+		}
+		if (!IsOnFloor())
+		{
+			Sprite.Play("air");
 		}
 
-		if (Input.IsActionJustPressed("jump") && IsOnFloor()) {
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
 			velocity.y = JUMPFORCE;
 		}
 

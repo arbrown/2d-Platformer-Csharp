f5402aa4a8cea3a46d808e02b912fe16f8fdb914
Drew Brown <drew@drewbrown.me>
2020-08-29 09:54:19 -0600

    Complete Part 17
    Add export presets for HTML5
    Change title screen background color
    Minor code formatting

--- coin.cs
+++ coin.cs
@@ -4,21 +4,21 @@ using System;
 public class coin : Area2D
 {
 
-    [Signal]
-    public delegate void CoinCollected();
+	[Signal]
+	public delegate void CoinCollected();
 
-    private void _on_coin_body_entered(object body)
-    {
-        GetNode<AnimationPlayer>("AnimationPlayer").Play("bounce");
-        EmitSignal("CoinCollected");
+	private void _on_coin_body_entered(object body)
+	{
+		GetNode<AnimationPlayer>("AnimationPlayer").Play("bounce");
+		EmitSignal("CoinCollected");
 
-        SetCollisionMaskBit(0, false);
+		SetCollisionMaskBit(0, false);
 
-        GetNode<AudioStreamPlayer>("SoundCoinCollect").Play();
-    }
-    private void _on_AnimationPlayer_animation_finished(String anim_name)
-    {
-        QueueFree();
-    }
+		GetNode<AudioStreamPlayer>("SoundCoinCollect").Play();
+	}
+	private void _on_AnimationPlayer_animation_finished(String anim_name)
+	{
+		QueueFree();
+	}
 }
 

9e4e4222ff66579d9f7dc77a4067483625fd4a1e
Drew Brown <drew@drewbrown.me>
2022-01-22 10:55:37 -0700

    Complete Part 18
    Implement finite state machine for walking, running and falling
    Refactor Steve.cs a bit
    Smooth out acceleration with Lerp calls

--- Steve.cs
+++ Steve.cs
@@ -4,50 +4,118 @@ using System;
 public class Steve : KinematicBody2D
 {
 	const int SPEED = 270;
+	const int RUNSPEED = 400;
 	const int GRAVITY = 35;
 	const int JUMPFORCE = -1100;
 
+	private State state = State.Air;
 	private int coins = 0;
 	private Vector2 velocity;
 
-	public AnimatedSprite Sprite { get => GetNode<AnimatedSprite>("Sprite"); }
+	public AnimatedSprite Sprite
+	{
+		get => GetNode<AnimatedSprite>("Sprite");
+	}
+
 	public override void _PhysicsProcess(float delta)
 	{
-		if (Input.IsActionPressed("right"))
+		Console.WriteLine(velocity.x);
+		switch (state)
 		{
-			velocity.x = SPEED;
-			Sprite.Play("walk");
-			Sprite.FlipH = false;
-		}
-		else if (Input.IsActionPressed("left"))
-		{
-			velocity.x = -SPEED;
-			Sprite.Play("walk");
-			Sprite.FlipH = true;
-		}
-		else
-		{
-			Sprite.Play("idle");
+			case State.Air:
+				if (IsOnFloor())
+				{
+					state = State.Floor;
+					goto case State.Floor; // Any other way to continue processing on this cycle?
+				}
 
-		}
-		if (!IsOnFloor())
-		{
-			Sprite.Play("air");
-		}
+				Sprite.Play("air");
+				if (Input.IsActionPressed("right"))
+				{
+					velocity.x = velocity.x < SPEED
+						? Mathf.Lerp(velocity.x, SPEED, 0.1f)
+						: Mathf.Lerp(velocity.x, SPEED, 0.03f);
+					Sprite.FlipH = false;
+				}
+				else if (Input.IsActionPressed("left"))
+				{
+					velocity.x = velocity.x > -SPEED // Note the flipped check for -SPEED
+						? Mathf.Lerp(velocity.x, -SPEED, 0.1f)
+						: Mathf.Lerp(velocity.x, -SPEED, 0.03f);
+					Sprite.FlipH = true;
+				}
+				else
+				{
+					velocity.x = Mathf.Lerp(velocity.x, 0, 0.2f);
+				}
 
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
-		{
-			velocity.y = JUMPFORCE;
-			GetNode<AudioStreamPlayer>("SoundJump").Play();
-		}
+				MoveAndFall();
+				break;
+			case State.Floor:
+				if (!IsOnFloor())
+				{
+					state = State.Air;
+				}
 
+				if (Input.IsActionPressed("right"))
+				{
+					if (Input.IsActionPressed("run"))
+					{
+						velocity.x = Mathf.Lerp(velocity.x, RUNSPEED, 0.1f);
+						Sprite.SpeedScale = 1.8f;
+					}
+					else
+					{
+						velocity.x = Mathf.Lerp(velocity.x, SPEED, 0.1f);
+						Sprite.SpeedScale = 1.0f;
+					}
+
+					Sprite.Play("walk");
+					Sprite.FlipH = false;
+				}
+				else if (Input.IsActionPressed("left"))
+				{
+					if (Input.IsActionPressed("run"))
+					{
+						velocity.x = Mathf.Lerp(velocity.x, -RUNSPEED, 0.1f);
+						Sprite.SpeedScale = 1.8f;
+					}
+					else
+					{
+						velocity.x = Mathf.Lerp(velocity.x, -SPEED, 0.1f);
+						Sprite.SpeedScale = 1.0f;
+					}
+
+					Sprite.Play("walk");
+					Sprite.FlipH = true;
+				}
+				else
+				{
+					Sprite.Play("idle");
+					velocity.x = Mathf.Lerp(velocity.x, 0, 0.2f);
+				}
+
+				if (Input.IsActionJustPressed("jump"))
+				{
+					velocity.y = JUMPFORCE;
+					GetNode<AudioStreamPlayer>("SoundJump").Play();
+				}
+
+				MoveAndFall();
+				break;
+			case State.Ladder:
+				break;
+			case State.Wall:
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	private void MoveAndFall()
+	{
 		velocity.y += 30;
-
 		velocity = MoveAndSlide(velocity, Vector2.Up);
-
-		velocity.x = Mathf.Lerp(velocity.x, 0, 0.2f); // Need to specify that the final parameter is a float
-
-
 	}
 
 	private void _on_fallzone_body_entered(object body)
@@ -62,13 +130,13 @@ public class Steve : KinematicBody2D
 
 	public void Bounce()
 	{
-		velocity.y += (float)(JUMPFORCE * 0.7);
+		velocity.y += (float) (JUMPFORCE * 0.7);
 	}
 
 	public void Ouch(float posx)
 	{
 		this.Modulate = new Color(1F, 0.3F, 0.3F, 0.3F);
-		velocity.y = (float)(JUMPFORCE * 0.5);
+		velocity.y = (float) (JUMPFORCE * 0.5);
 		if (Position.x < posx)
 		{
 			velocity.x = -800;
@@ -90,3 +158,10 @@ public class Steve : KinematicBody2D
 	}
 }
 
+internal enum State
+{
+	Air = 1, // Jumping or falling
+	Floor, // Walking or running
+	Ladder, // Climbing
+	Wall, // Wall-jumping
+}
